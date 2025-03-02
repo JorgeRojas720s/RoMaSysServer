6ab81b9e626fcc1d03752a3e0bd716b2de36fd3a
JorgeRojas720s <jitorojas37@gmail.com>
2025-03-02 02:39:41 -0600

    test15

--- backend/api/routes/clients-routes.js
+++ backend/api/routes/clients-routes.js
@@ -3,11 +3,11 @@ import { getClients, addClient, updateClient, deleteClient, getClientById } from
 
 const router = express.Router();
 
-
-router.get("/", getClients);
-router.get("/:cli_id", getClientById); 
-router.post("/", addClient);
-router.put("/:cli_id", updateClient);
-router.delete("/:cli_id", deleteClient);
+// Definición de rutas
+router.get("/", getClients); // GET /api/clients
+router.get("/:cli_id", getClientById); // GET /api/clients/:cli_id
+router.post("/", addClient); // POST /api/clients
+router.put("/:cli_id", updateClient); // PUT /api/clients/:cli_id
+router.delete("/:cli_id", deleteClient); // DELETE /api/clients/:cli_id
 
 export default router;
