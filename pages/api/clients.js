e2ef5a62d2f05208763c3c752f73e24fecf0a0bf
JorgeRojas720s <jitorojas37@gmail.com>
2025-03-02 18:34:47 -0600

    test30

--- pages/api/clients.js
+++ pages/api/clients.js
@@ -1,14 +1,10 @@
-// pages/api/clients.js
-import fetch from 'node-fetch';
+import clientsRoutes from "../backend/api/routes/clients-routes";
 
 export default async function handler(req, res) {
-  const backendUrl = 'https://ro-ma-sys-server.vercel.app/backend/server'; // URL de tu servidor backend
-  const response = await fetch(backendUrl, {
-    method: req.method,
-    headers: req.headers,
-    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined,
-  });
-
-  const data = await response.json();
-  res.status(response.status).json(data);
+  try {
+    await clientsRoutes(req, res);
+  } catch (error) {
+    console.error("Error in /api/clients:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
