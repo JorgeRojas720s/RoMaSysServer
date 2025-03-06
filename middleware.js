e5cbe283752a401b5774b7db8a629fd94f7d15d1
JorgeRojas720s <jitorojas37@gmail.com>
2025-03-06 12:46:34 -0600

    protect backend

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher(['/admin(.*)', '/api(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if(protectedRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).)", "/(api|trpc)(.)"],
+};
